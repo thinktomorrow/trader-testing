707594caf9737f7bb39887597c7ef99369bb6e58
Ben Cavens <cavensben@gmail.com>
2025-09-23 10:21:20 +0200

    fix

--- src/Support/CatalogRepositories.php
+++ src/Support/CatalogRepositories.php
@@ -7,7 +7,7 @@ use Thinktomorrow\Trader\Application\Product\Grid\FlattenedTaxonIds;
 use Thinktomorrow\Trader\Application\Product\ProductDetail\ProductDetailRepository;
 use Thinktomorrow\Trader\Application\Taxon\Queries\TaxaSelectOptions;
 use Thinktomorrow\Trader\Application\Taxon\Queries\TaxonFilters;
-use Thinktomorrow\Trader\Application\Taxon\Redirect\RedirectRepository;
+use Thinktomorrow\Trader\Application\Taxon\Redirect\TaxonRedirectRepository;
 use Thinktomorrow\Trader\Application\Taxon\Tree\TaxonTreeRepository;
 use Thinktomorrow\Trader\Domain\Model\Product\ProductRepository;
 use Thinktomorrow\Trader\Domain\Model\Product\VariantRepository;
@@ -30,7 +30,7 @@ interface CatalogRepositories
 
     public function variantForCartRepository(): VariantForCartRepository;
 
-    public function taxonRedirectRepository(): RedirectRepository;
+    public function taxonRedirectRepository(): TaxonRedirectRepository;
 
     public function taxonFilters(): TaxonFilters;
 
